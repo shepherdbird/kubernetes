e5948184849a5ad5519cb11cf71305466e1792db
dawei <dawei@dawei-OptiPlex-3020.(none)>
2015-05-28 11:06:59 +0800

    update fluentdv3.json

--- cluster/ubuntu/deploy-logging/deploy-fluentd.sh
+++ cluster/ubuntu/deploy-logging/deploy-fluentd.sh
@@ -2,10 +2,11 @@
 kubectl get nodes | awk '{
   if(NR>=2){
     system("kubectl label nodes "$1" node_id=m"NR-1" --overwrite=true");
-    system("sed -i s/node_id:.*$/node_id:\\ m"NR-1"/g ./fluentdv3.yaml");
-    system("sed -i s/fluentd[0-9]+*$/fluentd"NR-1"/g ./fluentdv3.yaml");
-    system("sed -i s/fluentd-logging.*$/fluentd-logging"NR-1"/g ./fluentdv3.yaml");
-    system("./pos_file.sh "$1);
-    system("kubectl create -f ./fluentdv3.yaml");
+    system("sed -r -i s/m[0-9]+/m"NR-1"/g ./newfluentd.json");
+    system("sed -r -i s/fluentd[0-9]+/fluentd"NR-1"/g ./newfluentd.json");
+    system("sed -r -i s/fluentd-logging[0-9]+/fluentd-logging"NR-1"/g ./newfluentd.json");
+    system("./ex.sh "$1);
+    system("kubectl create -f ./newfluentd.json");
   }
 }'
+#system("./ex.sh "$1);
--- cluster/ubuntu/deploy-logging/pos_file.sh
+++ cluster/ubuntu/deploy-logging/pos_file.sh
@@ -6,7 +6,7 @@ spawn ssh vcap@$ip
 expect {
     "yes/no" {send "yes\r";exp_continue}
     "password" {send "password\r";exp_continue}
-    "login" {send "sudo mkdir -p /varlog && sudo touch -f  /varlog/es-kubelet.log.pos && sudo touch -f  /varlog/es-containers.log.pos\r";exp_continue}
+    "login" {send "sudo mkdir -p /varlog && sudo touch -f  /var/log/es-kubelet.log.pos && sudo touch -f  /var/log/es-containers.log.pos\r";exp_continue}
    eof exit
 }
 EOF
